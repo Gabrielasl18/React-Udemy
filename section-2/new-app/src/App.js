ec077ea42a0f682e2799d5ac4017667d46716e35
gabrielasl18 <gabriela.lacerda2003@outlook.com>
2023-02-04 16:52:10 -0300

    att

---
+++ section-2/new-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+} 
+
+export default App;
